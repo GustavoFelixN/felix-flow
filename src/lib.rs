6f117bab2e2d00a7afb6e95ebbb555d409fec8b5
Gustavo Felix <gfnascimento@inf.ufsm.br>
2024-04-11 21:16:18 -0300

    Refactoring exp struct

--- src/lib.rs
+++ src/lib.rs
@@ -40,12 +40,12 @@ pub struct Expr {
 }
 
 impl Expr {
-    pub fn new(s: &str) -> Self {
+    pub fn new(s: &str) -> (&str, Self) {
         let (s, lhs) = Number::new(s);
         let (s, op) = Op::new(s);
-        let (_, rhs) = Number::new(s);
+        let (s, rhs) = Number::new(s);
 
-        Self { lhs, rhs, op }
+        (s, Self { lhs, rhs, op })
     }
 }
 
@@ -82,11 +82,14 @@ mod tests {
     fn parse_one_plus_two() {
         assert_eq!(
             Expr::new("1+2"),
-            Expr {
-                lhs: Number(1),
-                rhs: Number(2),
-                op: Op::Add,
-            }
+            (
+                "",
+                Expr {
+                    lhs: Number(1),
+                    rhs: Number(2),
+                    op: Op::Add,
+                }
+            )
         )
     }
 }
