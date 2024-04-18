f61663b31857580e286a49b62a90b30d4521df1f
Gustavo Felix <gfnascimento@inf.ufsm.br>
2024-04-18 15:18:59 -0300

    Treating identifiers starting with numbers

--- src/utils.rs
+++ src/utils.rs
@@ -27,7 +27,17 @@ pub(crate) fn extract_whitespace(s: &str) -> (&str, &str) {
 }
 
 pub(crate) fn extract_ident(s: &str) -> (&str, &str) {
-    take_while(|c| c.is_ascii_alphanumeric(), s)
+    let input_starts_with_alphabetic = s
+        .chars()
+        .next()
+        .map(|c| c.is_ascii_alphabetic())
+        .unwrap_or(false);
+
+    if input_starts_with_alphabetic {
+        take_while(|c| c.is_ascii_alphanumeric(), s)
+    } else {
+        (s, "")
+    }
 }
 
 #[cfg(test)]
@@ -87,5 +97,10 @@ mod tests {
     #[test]
     fn extract_alphanumeric_ident() {
         assert_eq!(extract_ident("foobar1()"), ("()", "foobar1"));
+    }
+
+    #[test]
+    fn cannot_extract_ident_beggining_with_number() {
+        assert_eq!(extract_ident("123abc"), ("123abc", ""));
     }
 }
