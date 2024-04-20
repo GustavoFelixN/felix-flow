b71bb4ce305d2ace504a73e8159c0f10e60d6a9a
Gustavo Felix <gfnascimento@inf.ufsm.br>
2024-04-20 18:43:04 -0300

    Adding more treatment form empty blocks

--- src/block.rs
+++ src/block.rs
@@ -7,7 +7,9 @@ pub struct Block {
 
 impl Block {
     pub fn new(s: &str) -> Result<(&str, Self), String> {
-        let s = utils::tag("{}", s)?;
+        let s = utils::tag("{", s)?;
+        let (s, _) = utils::extract_whitespace(s);
+        let s = utils::tag("}", s)?;
         Ok((s, Block { stmts: Vec::new() }))
     }
 }
@@ -19,5 +21,10 @@ mod tests {
     #[test]
     fn parse_empty_block() {
         assert_eq!(Block::new("{}"), Ok(("", Block { stmts: Vec::new() })))
+    }
+
+    #[test]
+    fn parse_empty_block_with_whitespace() {
+        assert_eq!(Block::new("{   }"), Ok(("", Block { stmts: Vec::new() })))
     }
 }
