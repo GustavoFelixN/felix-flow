eb9f0d4e263f549963d20eb28c2e6f026d757c58
Gustavo Felix <gfnascimento@inf.ufsm.br>
2024-04-20 18:54:03 -0300

    Creating basic parsing of single statements

--- src/block.rs
+++ src/block.rs
@@ -1,21 +1,31 @@
-use crate::{stmt::Smt, utils};
+use crate::{stmt::Stmt, utils};
 
 #[derive(Debug, PartialEq)]
 pub struct Block {
-    pub stmts: Vec<Smt>,
+    pub stmts: Vec<Stmt>,
 }
 
 impl Block {
     pub fn new(s: &str) -> Result<(&str, Self), String> {
         let s = utils::tag("{", s)?;
         let (s, _) = utils::extract_whitespace(s);
+
+        let (s, stmts) = if let Ok((s, stmt)) = Stmt::new(s) {
+            (s, vec![stmt])
+        } else {
+            (s, Vec::new())
+        };
+
+        let (s, _) = utils::extract_whitespace(s);
         let s = utils::tag("}", s)?;
-        Ok((s, Block { stmts: Vec::new() }))
+        Ok((s, Block { stmts }))
     }
 }
 
 #[cfg(test)]
 mod tests {
+    use crate::expr::{Expr, Number};
+
     use super::*;
 
     #[test]
@@ -26,5 +36,18 @@ mod tests {
     #[test]
     fn parse_empty_block_with_whitespace() {
         assert_eq!(Block::new("{   }"), Ok(("", Block { stmts: Vec::new() })))
+    }
+
+    #[test]
+    fn parse_block_with_one_statements() {
+        assert_eq!(
+            Block::new("{ 5 }"),
+            Ok((
+                "",
+                Block {
+                    stmts: vec![Stmt::Expr(Expr::Number(Number(5)))]
+                }
+            ))
+        )
     }
 }
