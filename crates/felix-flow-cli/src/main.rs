4aaf388fea5e7b35ef63d1a11af2b12a90fa606e
Gustavo Felix <gfnascimento@inf.ufsm.br>
2024-04-21 14:21:06 -0300

    Creating basic input loop

--- crates/felix-flow-cli/src/main.rs
+++ crates/felix-flow-cli/src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+use std::io;
+
+fn main() -> io::Result<()> {
+    loop {
+        let mut input = String::new();
+        io::stdin().read_line(&mut input)?;
+
+        dbg!(input);
+    }
 }
