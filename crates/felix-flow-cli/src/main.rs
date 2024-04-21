af103ad1918bf09615989a63b2127604f426de53
Gustavo Felix <gfnascimento@inf.ufsm.br>
2024-04-21 15:21:23 -0300

    updating cli

--- crates/felix-flow-cli/src/main.rs
+++ crates/felix-flow-cli/src/main.rs
@@ -1,17 +1,17 @@
 use std::io::{self, Write};
 
-fn run(input: &str, env: &mut felix_flow::Env) -> Result<(), String> {
+fn run(input: &str, env: &mut felix_flow::Env) -> Result<Option<felix_flow::Val>, String> {
     let parse = felix_flow::parse(input).map_err(|msg| format!("Parse error: {}", msg))?;
 
     let evaluated = parse
         .eval(env)
         .map_err(|msg| format!("Evaluation error: {}", msg))?;
 
-    if evaluated != felix_flow::Val::Unit {
-        dbg!(evaluated);
+    if evaluated == felix_flow::Val::Unit {
+        Ok(None)
+    } else {
+        Ok(Some(evaluated))
     }
-
-    Ok(())
 }
 
 fn main() -> io::Result<()> {
@@ -29,7 +29,10 @@ fn main() -> io::Result<()> {
         stdin.read_line(&mut input)?;
 
         match run(input.trim(), &mut env) {
-            Ok(()) => {}
+            Ok(Some(val)) => {
+                writeln!(stdout, "{}", val)?;
+            }
+            Ok(None) => {}
             Err(msg) => {
                 writeln!(stderr, "{}", msg)?;
                 stderr.flush()?;
