fb6fc858fd875b3e5fccc9e56e3bcb2b4aaf1cb1
Gustavo Felix <gfnascimento@inf.ufsm.br>
2024-05-13 14:00:40 -0300

    Creating function definition statement

---
+++ crates/felix-flow/src/func_def.rs
@@ -0,0 +1,54 @@
+use crate::{stmt::Stmt, utils};
+
+#[derive(Debug, PartialEq)]
+pub(crate) struct FuncDef {
+    pub(crate) name: String,
+    pub(crate) params: Vec<String>,
+    pub(crate) body: Box<Stmt>,
+}
+
+impl FuncDef {
+    pub(crate) fn new(s: &str) -> Result<(&str, Self), String> {
+        let s = utils::tag("fn", s)?;
+        let (s, _) = utils::extract_whitespace1(s)?;
+
+        let (s, name) = utils::extract_ident(s)?;
+        let (s, _) = utils::extract_whitespace(s);
+
+        let s = utils::tag("=>", s)?;
+        let (s, _) = utils::extract_whitespace(s);
+
+        let (s, body) = Stmt::new(s)?;
+
+        Ok((
+            s,
+            Self {
+                name: name.to_string(),
+                params: Vec::new(),
+                body: Box::new(body),
+            }
+        ))
+
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::expr::{Block, Expr};
+
+    #[test]
+    fn parse_func_def_with_no_params_and_empty_body() {
+        assert_eq!(
+            FuncDef::new("fn nothing => {}"),
+            Ok((
+                "",
+                FuncDef {
+                    name: "nothing".to_string(),
+                    params: Vec::new(),
+                    body: Box::new(Stmt::Expr(Expr::Block(Block { stmts: Vec::new() })))
+                }
+            ))
+        )
+    }
+}
