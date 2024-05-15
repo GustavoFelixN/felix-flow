e429cfa4cce641ebd63632f84fce7db2cba68cc0
Gustavo Felix <gfnascimento@inf.ufsm.br>
2024-05-15 08:54:03 -0300

    Adding remaing tokens to lexer

--- crates/felix-flow/src/lexer.rs
+++ crates/felix-flow/src/lexer.rs
@@ -4,17 +4,118 @@ use logos::Logos;
 pub(super) enum SyntaxKind {
     #[regex(" +")]
     WhiteSpace,
+
+    #[token("fn")]
+    FnKw,
+
+    #[token("let")]
+    LetKw,
+
+    #[regex("[A-Za-z][A-Za-z0-9]*")]
+    Ident,
+
+    #[regex("[0-9]+")]
+    Number,
+
+    #[token("+")]
+    Plus,
+
+    #[token("-")]
+    Minus,
+
+    #[token("*")]
+    Star,
+
+    #[token("/")]
+    Slash,
+
+    #[token("=")]
+    Equals,
+
+    #[token("{")]
+    LBrace,
+
+    #[token("}")]
+    RBrace,
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
+    fn check(input: &str, kind: SyntaxKind) {
+        let mut lexer = SyntaxKind::lexer(input);
+
+        assert_eq!(lexer.next(), Some(Ok(kind)));
+        assert_eq!(lexer.slice(), input);
+    }
+
     #[test]
     fn lex_spaces() {
-        let mut lexer = SyntaxKind::lexer("  ");
+        check("  ", SyntaxKind::WhiteSpace);
+    }
 
-        assert_eq!(lexer.next(), Some(Ok(SyntaxKind::WhiteSpace)));
-        assert_eq!(lexer.slice(), "  ");
+    #[test]
+    fn lex_fn_keyword() {
+        check("fn", SyntaxKind::FnKw);
+    }
+
+    #[test]
+    fn lex_let_keyword() {
+        check("let", SyntaxKind::LetKw);
+    }
+
+    #[test]
+    fn lex_alphabetic_ident() {
+        check("abcd", SyntaxKind::Ident);
+    }
+
+    #[test]
+    fn lex_alphanumeric_ident() {
+        check("abcd123abc456", SyntaxKind::Ident);
+    }
+    #[test]
+    fn lex_mixed_case_identifier() {
+        check("ABCdef", SyntaxKind::Ident);
+    }
+
+    #[test]
+    fn lex_number() {
+        check("123456", SyntaxKind::Number);
+    }
+
+    #[test]
+    fn lex_plus() {
+        check("+", SyntaxKind::Plus);
+    }
+
+    #[test]
+    fn lex_minus() {
+        check("-", SyntaxKind::Minus);
+    }
+
+    #[test]
+    fn lex_star() {
+        check("*", SyntaxKind::Star);
+    }
+
+    #[test]
+    fn lex_slash() {
+        check("/", SyntaxKind::Slash);
+    }
+
+    #[test]
+    fn lex_equals() {
+        check("=", SyntaxKind::Equals);
+    }
+
+    #[test]
+    fn lex_left_brace() {
+        check("{", SyntaxKind::LBrace);
+    }
+
+    #[test]
+    fn lex_right_brace() {
+        check("}", SyntaxKind::RBrace);
     }
 }
