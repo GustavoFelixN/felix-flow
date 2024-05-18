761d3ddade3b2148301c6deb2e514297473c2e8e
Gustavo Felix <gfnascimento@inf.ufsm.br>
2024-05-17 23:10:35 -0300

    panicking when marker is dropped without beeing completed

--- crates/felix-flow/src/parser/marker.rs
+++ crates/felix-flow/src/parser/marker.rs
@@ -1,17 +1,26 @@
+use core::panic;
+
 use super::event::Event;
 use super::Parser;
 use crate::lexer::SyntaxKind;
+use drop_bomb::DropBomb;
 
 pub(super) struct Marker {
     pos: usize,
+    bomb: DropBomb,
 }
 
 impl Marker {
     pub(super) fn new(pos: usize) -> Self {
-        Self { pos }
+        Self {
+            pos,
+            bomb: DropBomb::new("Markers need to be completed"),
+        }
     }
 
-    pub(super) fn complete(self, p: &mut Parser, kind: SyntaxKind) {
+    pub(super) fn complete(mut self, p: &mut Parser, kind: SyntaxKind) {
+        self.bomb.defuse();
+
         let event_at_pos = &mut p.events[self.pos];
         assert_eq!(*event_at_pos, Event::Placeholder);
 
