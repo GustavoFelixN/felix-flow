dfe909043fed3eff30280ddb41189eae9a3455b3
Gustavo Felix <gfnascimento@inf.ufsm.br>
2024-05-21 11:37:38 -0300

    Creating intermediary structures for ast

--- crates/ast/src/lib.rs
+++ crates/ast/src/lib.rs
@@ -1,14 +1,143 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use smol_str::SmolStr;
+use syntax::{SyntaxElement, SyntaxKind, SyntaxNode, SyntaxToken};
+
+#[derive(Debug)]
+pub struct VariableDef(SyntaxNode);
+
+impl VariableDef {
+    pub fn name(&self) -> Option<SyntaxToken> {
+        self.0
+            .children_with_tokens()
+            .filter_map(SyntaxElement::into_token)
+            .find(|token| token.kind() == SyntaxKind::Ident)
+    }
+
+    pub fn value(&self) -> Option<Expr> {
+        self.0.children().find_map(Expr::cast)
+    }
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+#[derive(Debug)]
+pub enum Expr {
+    BinaryExpr(BinaryExpr),
+    Literal(Literal),
+    ParentExpr(ParentExpr),
+    UnaryExpr(UnaryExpr),
+    VariableRef(VariableRef),
+}
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+impl Expr {
+    pub fn cast(node: SyntaxNode) -> Option<Self> {
+        let result = match node.kind() {
+            SyntaxKind::InfixExpr => Self::BinaryExpr(BinaryExpr(node)),
+            SyntaxKind::Literal => Self::Literal(Literal(node)),
+            SyntaxKind::ParenExpr => Self::ParentExpr(ParentExpr(node)),
+            SyntaxKind::PrefixExpr => Self::UnaryExpr(UnaryExpr(node)),
+            SyntaxKind::VariableRef => Self::VariableRef(VariableRef(node)),
+            _ => return None,
+        };
+        Some(result)
+    }
+}
+
+#[derive(Debug)]
+pub struct BinaryExpr(SyntaxNode);
+
+impl BinaryExpr {
+    pub fn lhs(&self) -> Option<Expr> {
+        self.0.children().find_map(Expr::cast)
+    }
+
+    pub fn rhs(&self) -> Option<Expr> {
+        self.0.children().filter_map(Expr::cast).nth(1)
+    }
+
+    pub fn op(&self) -> Option<SyntaxToken> {
+        self.0
+            .children_with_tokens()
+            .filter_map(SyntaxElement::into_token)
+            .find(|token| {
+                matches!(
+                    token.kind(),
+                    SyntaxKind::Plus | SyntaxKind::Minus | SyntaxKind::Star | SyntaxKind::Slash
+                )
+            })
+    }
+}
+
+#[derive(Debug)]
+pub struct UnaryExpr(SyntaxNode);
+
+impl UnaryExpr {
+    pub fn expr(&self) -> Option<Expr> {
+        self.0.children().find_map(Expr::cast)
+    }
+
+    pub fn op(&self) -> Option<SyntaxToken> {
+        self.0
+            .children_with_tokens()
+            .filter_map(SyntaxElement::into_token)
+            .find(|token| matches!(token.kind(), SyntaxKind::Minus))
+    }
+}
+
+#[derive(Debug)]
+pub struct ParentExpr(SyntaxNode);
+
+impl ParentExpr {
+    pub fn expr(&self) -> Option<Expr> {
+        self.0.children().find_map(Expr::cast)
+    }
+}
+
+#[derive(Debug)]
+pub struct Literal(SyntaxNode);
+
+impl Literal {
+    pub fn parse(&self) -> u64 {
+        self.0.first_token().unwrap().text().parse().unwrap()
+    }
+}
+
+#[derive(Debug)]
+pub struct VariableRef(SyntaxNode);
+
+impl VariableRef {
+    pub fn name(&self) -> SmolStr {
+        self.0.first_token().unwrap().text().into()
+    }
+}
+
+#[derive(Debug)]
+pub enum Stmt {
+    VariableDef(VariableDef),
+    Expr(Expr),
+}
+
+impl Stmt {
+    pub fn cast(node: SyntaxNode) -> Option<Self> {
+        let result = match node.kind() {
+            SyntaxKind::VariableDef => Self::VariableDef(VariableDef(node)),
+            _ => Self::Expr(Expr::cast(node)?),
+        };
+
+        Some(result)
+    }
+}
+
+#[derive(Debug)]
+pub struct Root(SyntaxNode);
+
+impl Root {
+    pub fn cast(node: SyntaxNode) -> Option<Self> {
+        if node.kind() == SyntaxKind::Root {
+            Some(Self(node))
+        } else {
+            None
+        }
+    }
+
+    pub fn stmts(&self) -> impl Iterator<Item = Stmt> {
+        self.0.children().filter_map(Stmt::cast)
     }
 }
