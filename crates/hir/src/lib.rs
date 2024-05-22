b733d07bdb8305783406b187a3bf2bbb73e4bfdb
Gustavo Felix <gfnascimento@inf.ufsm.br>
2024-05-22 10:42:20 -0300

    Implementing basic data structure to hir

---
+++ crates/hir/src/lib.rs
@@ -0,0 +1,40 @@
+use smol_str::SmolStr;
+
+#[derive(Debug)]
+pub enum Stmt {
+    VariableDef { name: SmolStr, value: Expr },
+    Expr(Expr),
+}
+
+#[derive(Debug)]
+pub enum Expr {
+    Missing,
+    Binary {
+        op: BinaryOp,
+        lhs: Box<Self>,
+        rhs: Box<Self>,
+    },
+    Literal {
+        n: u64,
+    },
+    Unary {
+        op: UnaryOp,
+        expr: Box<Self>,
+    },
+    VariableRef {
+        var: SmolStr,
+    },
+}
+
+#[derive(Debug)]
+pub enum BinaryOp {
+    Add,
+    Sub,
+    Mul,
+    Div,
+}
+
+#[derive(Debug)]
+pub enum UnaryOp {
+    Neg,
+}
